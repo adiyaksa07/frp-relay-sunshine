54ba7b03f82d72e589ba0912aee0f16dc45489f5
Ubuntu <ubuntu@ip-172-31-29-192.ap-southeast-1.compute.internal>
2025-04-06 23:20:59 +0000

    Remove default folder & update optimize_network.sh

--- optimize_network.sh
+++ optimize_network.sh
@@ -22,32 +22,34 @@ apply_kernel_optimizations() {
                  tcp_low_latency tcp_fin_timeout tcp_tw_reuse somaxconn \
                  tcp_congestion_control default_qdisc tcp_fastopen \
                  netdev_max_backlog tcp_max_syn_backlog tcp_syncookies \
-                 ip_local_port_range
+                 ip_local_port_range tcp_mtu_probing tcp_slow_start_after_idle \
+		 tcp_notsent_lowat
     do
         sed -i "/^net\.ipv4\.$param/d" /etc/sysctl.conf
         sed -i "/^net\.core\.$param/d" /etc/sysctl.conf
     done
 
     cat >> /etc/sysctl.conf <<EOF
-net.core.rmem_max=4194304
-net.core.wmem_max=4194304
-net.ipv4.tcp_rmem=4096 87380 4194304
-net.ipv4.tcp_wmem=4096 16384 4194304
-net.ipv4.tcp_window_scaling=1
-net.ipv4.tcp_sack=1
-net.ipv4.tcp_low_latency=1
-net.ipv4.tcp_fin_timeout=30
-net.ipv4.tcp_tw_reuse=1
-net.core.somaxconn=65535
-net.ipv4.tcp_congestion_control=bbr
-net.core.default_qdisc=fq_codel
-tcp_mtu_probing=1
-net.ipv4.tcp_fastopen=3
-net.core.netdev_max_backlog=5000
-net.ipv4.tcp_max_syn_backlog=8192
-net.ipv4.tcp_syncookies=1
-net.ipv4.ip_local_port_range=1024 65535
-tcp_slow_start_after_idle=0
+net.core.rmem_max = 4194304
+net.core.wmem_max = 4194304
+net.ipv4.tcp_rmem = 4096 87380 4194304
+net.ipv4.tcp_wmem = 4096 16384 4194304
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_low_latency = 1
+net.ipv4.tcp_fin_timeout = 10
+net.ipv4.tcp_tw_reuse = 1
+net.core.somaxconn = 65535
+net.ipv4.tcp_congestion_control = bbr
+net.core.default_qdisc = fq_codel
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_fastopen = 3
+net.core.netdev_max_backlog = 5000
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_syncookies = 1
+net.ipv4.ip_local_port_range = 1024 65535
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_notsent_lowat = 16384
 EOF
 
     sysctl -p
